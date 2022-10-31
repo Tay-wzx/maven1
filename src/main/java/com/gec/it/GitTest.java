4b148b55e1d53530029555d6873f5b36938f326a
wzx <wzx36543111@163.com>
2022-10-31 17:42:10 +0800

    third commit

--- src/main/java/com/gec/it/GitTest.java
+++ src/main/java/com/gec/it/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello,world!");
         System.out.println("这是第一个程序");
+        System.out.println("这是第二个版本");
     }
 }
