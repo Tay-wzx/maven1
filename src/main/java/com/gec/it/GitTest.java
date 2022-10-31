f098215077019160dac8f12d88724097c738e4f9
wzx <wzx36543111@163.com>
2022-10-31 17:41:11 +0800

    second commit

--- src/main/java/com/gec/it/GitTest.java
+++ src/main/java/com/gec/it/GitTest.java
@@ -1,7 +1,13 @@
 package com.gec.it;
-
+/**
+ *  GitTest
+ *
+ * @author Wzx
+ * @date 2022/10/31
+ */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello,world!");
+        System.out.println("这是第一个程序");
     }
 }
