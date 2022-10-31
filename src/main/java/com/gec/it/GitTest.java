999291de1c6a7ea68af2c527bc86ad69539074da
wzx <wzx36543111@163.com>
2022-10-31 20:55:23 +0800

    bugFix:3.0

--- src/main/java/com/gec/it/GitTest.java
+++ src/main/java/com/gec/it/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello,world!");
         System.out.println("修改后第一版本");
         System.out.println("修改后的第二个版本");
+        System.out.println("{bugFix}修改3.0");
     }
 }
