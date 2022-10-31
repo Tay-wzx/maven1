b2ad2de9712b501c8b484e7c1cfeb846a2f824cd
wzx <wzx36543111@163.com>
2022-10-31 20:44:02 +0800

    fix commit

--- src/main/java/com/gec/it/GitTest.java
+++ src/main/java/com/gec/it/GitTest.java
@@ -3,5 +3,6 @@ package com.gec.it;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello,world!");
+        System.out.println("修改后第一版本");
     }
 }
